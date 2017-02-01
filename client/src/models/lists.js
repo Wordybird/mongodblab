ce88acac9228306dbe9bc8a110a757cde685c787
chrisbacon <bacon.chris@gmail.com>
2017-02-01 13:43:04 +0000

    changed lists.js all to make call to /api/lists

--- client/src/models/lists.js
+++ client/src/models/lists.js
@@ -17,8 +17,35 @@ var Lists = function() {
 
 Lists.prototype = {
 
+    makeRequest: function (url, callback) {
+      var request = new XMLHttpRequest();
+      request.open('GET', url);
+      request.onload = callback;
+      request.send();
+    },
+
+    populateLists: function(results) {
+        var lists = [];
+        for (var result of results) {
+            var list = new List(result);
+            lists.push(list);
+        }
+        return lists;
+    },   
+
     all: function(callback) {
-        callback(this.lists);
+        var self = this;
+
+        this.makeRequest('http://localhost:3000/api/lists', function() {
+            if (this.status !== 200) {
+                return;
+            }
+            var jsonString = this.responseText;
+            var results = JSON.parse(jsonString);
+
+            var lists = self.populateLists(results);
+            callback(lists);
+        });
     },
 
     find: function(listName, callback) {
